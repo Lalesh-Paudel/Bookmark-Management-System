5f05c89407ebb3df9d516742cd741e806457e997
Manish Pandey <np03a190132@heraldcollege.edu.np>
2021-05-17 21:40:15 +0545

    Backend of Login and Registration Page

--- bms/login/views.py
+++ bms/login/views.py
@@ -4,7 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 def login(request):
-    return render(request, 'login1.html')
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login1.html',{'form':form})
 
 
 def authenticate(request):
--- bms/register/views.py
+++ bms/register/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
 def render_register_from(request):
-    return render(request, 'Register1.html')
+    if request.method == 'POST':
+        # form = UserCreationForm(request.POST)
+        print ("Lauda lassan jhfjshjhdfj sfhjshdjhskjdhjks d working")
+    # if form.is_valid():
+    #     form.save()
+    #     return redirect('login')
+    # else:
+    #     form = UserCreationForm()
+    return render(request, 'Register1.html',{})
 
 def register(request):
     full_name = request.POST['fullName']
